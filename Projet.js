2771f42355dc5ddefb1677ffaa882c5128de0249
MaximeAntoine14250 <maxime.antoine.brouay@gmail.com>
2025-03-27 11:50:04 +0100

    27/03/25 1er commit

---
+++ Projet.js
@@ -0,0 +1,13 @@
+const url =("https://geo.api.gouv.fr/communes?codePostal=",elt)
+
+
+/*
+fetch(url)
+.then(response => response.json())
+.then(data => {
+    let adviceElement = document.createElement('p');
+
+})
+*/
+
+
